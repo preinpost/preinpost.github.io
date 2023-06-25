18c0e707211d129b06b8889e199ec2aef435694d
preinpost <>
2023-06-25 06:58:09 +0000

    title image 추가

--- _next/static/chunks/app/blog/detail/[id]/page-cfa2524fbb91c6d9.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[958],{4949:function(e,t,r){Promise.resolve().then(r.bind(r,6427)),Promise.resolve().then(r.bind(r,5566))},5566:function(e,t,r){"use strict";r.r(t),r.d(t,{default:function(){return s}});var n=r(9268);function s(e){let{children:t}=e;return(0,n.jsx)("div",{className:"flex flex-col w-4/5 max-w-4xl self-center",children:t})}},6427:function(e,t,r){"use strict";r.r(t),r.d(t,{default:function(){return s}});var n=r(9268);function s(){return(0,n.jsx)("section",{className:"mt-32",ref:e=>{if(!e)return;let t=document.createElement("script");t.src="https://utteranc.es/client.js",t.async=!0,t.setAttribute("repo","preinpost/preinpost.github.io"),t.setAttribute("issue-term","pathname"),t.setAttribute("theme","github-dark"),t.crossOrigin="anonymous",e.appendChild(t)}})}},3177:function(e,t,r){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=r(6006),s=Symbol.for("react.element"),o=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),i=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,u={key:!0,ref:!0,__self:!0,__source:!0};function c(e,t,r){var n,c={},f=null,l=null;for(n in void 0!==r&&(f=""+r),void 0!==t.key&&(f=""+t.key),void 0!==t.ref&&(l=t.ref),t)o.call(t,n)&&!u.hasOwnProperty(n)&&(c[n]=t[n]);if(e&&e.defaultProps)for(n in t=e.defaultProps)void 0===c[n]&&(c[n]=t[n]);return{$$typeof:s,type:e,key:f,ref:l,props:c,_owner:i.current}}t.jsx=c,t.jsxs=c},9268:function(e,t,r){"use strict";e.exports=r(3177)}},function(e){e.O(0,[667,488,744],function(){return e(e.s=4949)}),_N_E=e.O()}]);
