f0b2e5b3984a3983d36c8e39519e340fd0efc4f4
preinpost <>
2024-01-06 04:46:16 +0000

    레이아웃 수정

--- _next/static/chunks/app/about/page-0cebb6c3b148d1c6.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301],{3970:function(e,r,n){Promise.resolve().then(n.bind(n,2195))},2195:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return o}});var t=n(9268);function o(e){let{children:r}=e;return(0,t.jsx)("div",{className:"flex flex-col w-11/12 max-w-4xl self-center md:w-4/5",children:r})}},3177:function(e,r,n){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var t=n(6006),o=Symbol.for("react.element"),f=Symbol.for("react.fragment"),s=Object.prototype.hasOwnProperty,u=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function l(e,r,n){var t,f={},l=null,i=null;for(t in void 0!==n&&(l=""+n),void 0!==r.key&&(l=""+r.key),void 0!==r.ref&&(i=r.ref),r)s.call(r,t)&&!c.hasOwnProperty(t)&&(f[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===f[t]&&(f[t]=r[t]);return{$$typeof:o,type:e,key:l,ref:i,props:f,_owner:u.current}}r.Fragment=f,r.jsx=l,r.jsxs=l},9268:function(e,r,n){"use strict";e.exports=n(3177)}},function(e){e.O(0,[667,488,744],function(){return e(e.s=3970)}),_N_E=e.O()}]);
